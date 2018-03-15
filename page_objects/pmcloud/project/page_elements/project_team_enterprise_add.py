31465603f670b2224a303d18474f38a1983b17d7
zxcyui369 <sws369@126.com>
2018-03-15 14:36:44 +0800

    edit project team enterprise add add project team enterprise add actions

--- page_objects/pmcloud/project/page_elements/project_team_enterprise_add.py
+++ page_objects/pmcloud/project/page_elements/project_team_enterprise_add.py
@@ -74,38 +74,67 @@ class ProjectTeamEnterprise:
     ENTERPRISELISTMSG = ("xpath", "html/body/div[4]/div/div/div/div/div[2]/div/div[2]/div/div/div")  # 参与方列表提示信息
 
     # 单行参与方
-    SINGLEENTERPRISETYPE2 = ("xpath", )
-    SINGLEENTERPRISENAME2 = ("xpath", )
-    SINGLEENTERPRISEOPERATION2 = ("xpath", )
+    SINGLEENTERPRISENAME3 = ("xpath", ENTERPRISECOMMONXPATH3 + "/div[2]/div/div[2]/div/div/span/div[2]/table/tbody/tr/td[2]")
+    SINGLEENTERPRISEOPERATION3 = ("xpath", ENTERPRISECOMMONXPATH3 + "/div[2]/div/div[2]/div/div/span/div[2]/table/tbody/tr/td[3]")
 
-    SINGLEENTERPRISETYPE3 = ("xpath", )
-    SINGLEENTERPRISENAME3 = ("xpath", )
-    SINGLEENTERPRISEOPERATION3 = ("xpath", )
+    SINGLEENTERPRISENAME4 = ("xpath", ENTERPRISECOMMONXPATH4 + "/div[2]/div/div[2]/div/div/span/div[2]/table/tbody/tr/td[2]")
+    SINGLEENTERPRISEOPERATION4 = ("xpath", ENTERPRISECOMMONXPATH4 + "/div[2]/div/div[2]/div/div/span/div[2]/table/tbody/tr/td[3]")
 
     # 多行参与方
     # 第一个参与方信息
-    # 参与方类型控件组
-    FIRSTENTERPRISETYPE2 = ("xpath", getEnterpriseTypeXpathByIndex(index=1, div=2))  # 参与方类型
+    FIRSTENTERPRISENAME3 = ("xpath", getEnterpriseAddNameXpathByIndex(index=1, div=3))
+    FIRSTENTERPRISEOPERATION3 = ("xpath", getEnterpriseAddOperationXpathByIndex(index=1, div=3) + "/a/span")
 
-    FIRSTENTERPRISENAME2 = ("xpath", getEnterpriseTypeXpathByIndex(index=1, div=2))  # 参与方企业名称
-
-    FIRSTENTERPRISETYPE3 = ("xpath", getEnterpriseTypeXpathByIndex(index=1, div=3))  # 参与方类型
-    FIRSTENTERPRISENAME3 = ("xpath", getEnterpriseTypeXpathByIndex(index=1, div=3))  # 参与方企业名称
+    FIRSTENTERPRISENAME4 = ("xpath", getEnterpriseAddNameXpathByIndex(index=1, div=4))
+    FIRSTENTERPRISEOPERATION4 = ("xpath", getEnterpriseAddOperationXpathByIndex(index=1, div=4) + "/a/span")
 
     # 第二个参与方信息
+    SECONDENTERPRISENAME3 = ("xpath", getEnterpriseAddNameXpathByIndex(index=2, div=3))
+    SECONDENTERPRISEOPERATION3 = ("xpath", getEnterpriseAddOperationXpathByIndex(index=2, div=3) + "/a/span")
+
+    SECONDENTERPRISENAME4 = ("xpath", getEnterpriseAddNameXpathByIndex(index=2, div=4))
+    SECONDENTERPRISEOPERATION4 = ("xpath", getEnterpriseAddOperationXpathByIndex(index=2, div=4) + "/a/span")
+
+    # 参与方类型控件组
+    # 单行参与方
+    SINGLEENTERPRISETYPE3 = ("xpath", ENTERPRISECOMMONXPATH3 + "/div[2]/div/div[2]/div/div/span/div[2]/table/tbody/tr/td[1]/div/div/span")
+    SINGLEENTERPRISETYPESELECTYZ4 = ("xpath", "html/body/div[4]/div/div/div/ul/li[1]")  # 选择业主方
+    SINGLEENTERPRISETYPESELECTSG4 = ("xpath", "html/body/div[4]/div/div/div/ul/li[2]")  # 选择施工方
+    SINGLEENTERPRISETYPESELECTJL4 = ("xpath", "html/body/div[4]/div/div/div/ul/li[3]")  # 选择监理方
+
+    SINGLEENTERPRISETYPE4 = ("xpath", ENTERPRISECOMMONXPATH4 + "/div[2]/div/div[2]/div/div/span/div[2]/table/tbody/tr/td[1]/div/div/span")
+    SINGLEENTERPRISETYPESELECTYZ5 = ("xpath", "html/body/div[5]/div/div/div/ul/li[1]")  # 选择业主方
+    SINGLEENTERPRISETYPESELECTSG5 = ("xpath", "html/body/div[5]/div/div/div/ul/li[2]")  # 选择施工方
+    SINGLEENTERPRISETYPESELECTJL5 = ("xpath", "html/body/div[5]/div/div/div/ul/li[3]")  # 选择监理方
+
+    # 多行参与方
+    # 第一个参与方
+    FIRSTENTERPRISETYPE3 = ("xpath", getEnterpriseAddTypeXpathByIndex(index=1, div=3) + "/div/div/span")
+    FIRSTENTERPRISETYPESELECTYZ4 = ("xpath", "html/body/div[4]/div/div/div/ul/li[1]")  # 选择业主方
+    FIRSTENTERPRISETYPESELECTSG4 = ("xpath", "html/body/div[4]/div/div/div/ul/li[2]")  # 选择施工方
+    FIRSTENTERPRISETYPESELECTJL4 = ("xpath", "html/body/div[4]/div/div/div/ul/li[3]")  # 选择监理方
+
+    FIRSTENTERPRISETYPE4 = ("xpath", getEnterpriseAddTypeXpathByIndex(index=1, div=4) + "/div/div/span")
+    FIRSTENTERPRISETYPESELECTYZ5 = ("xpath", "html/body/div[5]/div/div/div/ul/li[1]")  # 选择业主方
+    FIRSTENTERPRISETYPESELECTSG5 = ("xpath", "html/body/div[5]/div/div/div/ul/li[2]")  # 选择施工方
+    FIRSTENTERPRISETYPESELECTJL5 = ("xpath", "html/body/div[5]/div/div/div/ul/li[3]")  # 选择监理方
+
+    # 第二个参与方
+    SECONDENTERPRISETYPE3 = ("xpath", getEnterpriseAddTypeXpathByIndex(index=2, div=3) + "/div/div/span")
+    SECONDENTERPRISETYPESELECTYZ5 = ("xpath", "html/body/div[5]/div/div/div/ul/li[1]")  # 选择业主方
+    SECONDENTERPRISETYPESELECTSG5 = ("xpath", "html/body/div[5]/div/div/div/ul/li[2]")  # 选择施工方
+    SECONDENTERPRISETYPESELECTJL5 = ("xpath", "html/body/div[5]/div/div/div/ul/li[3]")  # 选择监理方
+
+    SECONDENTERPRISETYPE4 = ("xpath", getEnterpriseAddTypeXpathByIndex(index=2, div=4) + "/div/div/span")
+    SECONDENTERPRISETYPESELECTYZ6 = ("xpath", "html/body/div[6]/div/div/div/ul/li[1]")  # 选择业主方
+    SECONDENTERPRISETYPESELECTSG6 = ("xpath", "html/body/div[6]/div/div/div/ul/li[2]")  # 选择施工方
+    SECONDENTERPRISETYPESELECTJL6 = ("xpath", "html/body/div[6]/div/div/div/ul/li[3]")  # 选择监理方
 
     # 标题行
-    MODULENAME2 = ("xpath", ENTERPRISECOMMONXPATH2 + "/div[1]/h4")  # 模块名称
-    DELETEBTN2 = ("xpath", ENTERPRISECOMMONXPATH2 + "/div[2]/div[1]/button[1]")  # 删除按钮
-    ADDBTN2 = ("xpath", ENTERPRISECOMMONXPATH2 + "/div[2]/div[1]/button[2]")  # 添加按钮
-    CLOSEBTN2 = ("xpath", ENTERPRISECOMMONXPATH2 + "/div[1]/button")  # 右上角关闭按钮
-
-    MODULENAME3 = ("xpath", ENTERPRISECOMMONXPATH3 + "/div[1]/h4")  # 模块名称
-    DELETEBTN3 = ("xpath", ENTERPRISECOMMONXPATH3 + "/div[2]/div[1]/button[1]")  # 删除按钮
-    ADDBTN3 = ("xpath", ENTERPRISECOMMONXPATH3 + "/div[2]/div[1]/button[2]")  # 添加按钮
+    MODULENAME3 = ("xpath", ENTERPRISECOMMONXPATH3 + "/div[1]")  # 模块名称
     CLOSEBTN3 = ("xpath", ENTERPRISECOMMONXPATH3 + "/div[1]/button")  # 右上角关闭按钮
 
 
+    MODULENAME4 = ("xpath", ENTERPRISECOMMONXPATH4 + "/div[1]")  # 模块名称
+    CLOSEBTN4 = ("xpath", ENTERPRISECOMMONXPATH4 + "/div[1]/button")  # 右上角关闭按钮
 
-
-    # 参与方选项
