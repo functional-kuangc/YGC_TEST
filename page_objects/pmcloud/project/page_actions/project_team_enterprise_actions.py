1ad3204d3f5b76e0c362769527674a1eeea7d901
zxcyui369 <sws369@126.com>
2018-03-15 17:09:07 +0800

    edit project team enterprise actions

---
+++ page_objects/pmcloud/project/page_actions/project_team_enterprise_actions.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+# 导入公共函数Selenium封装类
+from libs.pagepubselenium import PagePubSelenium
+# 导入项目参与方元素定位
+from page_objects.pmcloud.project.page_elements.project_team_enterprise import ProjectTeamEnterprise
+# 导入页面等待装饰器
+from libs.timedecorator import timeDecorator
+
+
+class ProjectTeamEnterpriseActions(PagePubSelenium):
+    """项目参与方操作类"""
+    pass
---
+++ page_objects/pmcloud/project/page_actions/project_team_enterprise_add_actions.py
@@ -0,0 +1,12 @@
+# coding: utf-8
+# 导入公共函数Selenium封装类
+from libs.pagepubselenium import PagePubSelenium
+# 导入添加参与企业元素定位
+from page_objects.pmcloud.project.page_elements.project_team_enterprise_add import ProjectTeamEnterpriseAdd
+# 导入页面等待装饰器
+from libs.timedecorator import timeDecorator
+
+
+class ProjectTeamEnterpriseAddActions(PagePubSelenium):
+    """添加参与企业操作类"""
+    pass
